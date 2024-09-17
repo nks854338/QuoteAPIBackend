215e5ad0f41895b2375886e88bceeb7c8296930d
Nandani <nks854338@gmail.com>
2024-09-17 11:00:17 +0530

    commited

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const quotes_routes = require("./routes/quotes");
+require("dotenv").config();
+const app = express();
+const connectDB = require("./db/connect");
+const cors = require("cors");
+
+//middlewares
+app.use(express.urlencoded({ extended: false }));
+//middleware to set router
+app.use("/api/quotes", quotes_routes);
+app.use(quotes_routes);
+
+const port = process.env.port || 6600;
+
+app.use(cors());
+y
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`server started at port:${port}`);
+    });
+    await connectDB(process.env.MONGODB_URL);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
